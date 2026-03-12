09a49b0403b80048e5f268ae3d451c740e574d49
IgorSabadini0 <igorsabadini157@gmail.com>
2026-03-11 21:39:18 -0300

    fix: syntax errors

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { View, Text, Image, TextInput } from "react-native";
 import Logo from "../../assets/logo_etec.png";
-import Styles from "./styles"
+import { style } from "./styles";
 
 export default function login() {
     return (
-        <View style={Styles.container}>
+        <View style={style.container}>
             <View> {/* Logo */}
                 <Image source={Logo} resizeMode="contain" />
                 <Text>Bem Vindo de Volta</Text>
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -2,6 +2,14 @@ import { Dimensions, StyleSheet } from 'react-native';
 import { themas } from "../../global/themes";
 export const style = StyleSheet.create({
     container: {
-        color: 
-    }
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    boxTop: {
+        height: Dimensions.get("window").height / 3,
+        width: '100%',
+        justifyContent: "center",
+        alignItems: "center"
+    },
 });
